662dfd07e1e848b3a73772b34b65aa5b3cdfd74c
Yuliia <67199866+yuliiagorgozka@users.noreply.github.com>
2023-06-12 20:14:52 +0300

    Daos 562 (#48)
    * DAOS-562
    
    * DAOS-559
    
    * remove console log
    
    * fix-559
    
    * DAOS-555

--- components/CreateAgreement/TextEditor/index.tsx
+++ components/CreateAgreement/TextEditor/index.tsx
@@ -148,7 +148,7 @@ const TextEditor = ({
               }}
               sx={expandBtn}
             >
-              Expand
+              {!expand ? "Expand" : "Collapse"}
             </Button>
           )}
         </Flex>
