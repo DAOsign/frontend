8ba0694ae04f8d49aec1b7025f12545281587885
Yuliia <taragorgodze@gmail.com>
2023-07-26 11:52:23 +0300

    add Profile

---
+++ components/Profile/Info/styles.ts
@@ -0,0 +1,21 @@
+import { ThemeUIStyleObject } from "theme-ui";
+
+export const container = {
+  flexDirection: "column",
+  p: "40px",
+
+  //   "@media screen and (max-width: 719px)": {
+  //     m: "0 auto",
+  //   },
+} as ThemeUIStyleObject;
+
+export const title = {
+  variant: "text.overscript",
+  width: "55px",
+} as ThemeUIStyleObject;
+
+export const subTitle = {
+  variant: "text.smallTextMediumUser",
+  width: "400px",
+} as ThemeUIStyleObject;
+subTitle;
--- components/Profile/index.tsx
+++ components/Profile/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import Info from "./Info";
+import { useWeb3 } from "../../hooks/useWeb3";
 import {} from "./styles";
 
 export default function Profile() {
-  return <>Profile</>;
+  const { account } = useWeb3();
+  return (
+    <>
+      <Info address={account || ""} />
+    </>
+  );
 }
