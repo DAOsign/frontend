ea93becee873c88bee5814a019f0cdb38be5d485
Yuliia <taragorgodze@gmail.com>
2023-08-16 14:42:22 +0300

    fix wallet

--- components/Profile/UserName.tsx
+++ components/Profile/UserName.tsx
@@ -3,7 +3,7 @@ import { Flex, Text } from "theme-ui";
 import { useWeb3 } from "../../hooks/useWeb3";
 import { SocialLink } from "../PublicProfile/SocialLink";
 import WalletAddress from "./WalletAddress";
-import { userName, containerName } from "./styles";
+import { title, userName, containerName } from "./styles";
 
 const UserName = () => {
   const { account } = useWeb3();
@@ -12,7 +12,10 @@ const UserName = () => {
       <Flex sx={containerName}>
         <Text sx={userName}>johndoe.eth</Text>
         <SocialLink />
-        <WalletAddress address={account || ""} />
+        <Flex>
+          <Text sx={{ ...title, minWidth: "55px" }}>Address</Text>
+          <WalletAddress address={account || ""} />
+        </Flex>
       </Flex>
     </>
   );
