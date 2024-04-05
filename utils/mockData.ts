80f1f66c32f49dcc60e1a8a6b673726cb43fd5b6
Brioline <mikelrap@gmail.com>
2024-04-05 15:01:48 +0300

    goreli to sepolia storage

--- utils/mockData.ts
+++ utils/mockData.ts
@@ -5,7 +5,7 @@ import OasisImg from "../img/svg/mainnet/oasis.svg";
 import { Option } from "../components/Select";
 
 export const networkOptions: Option[] = [
-  { label: "Goerli (ETH Testnet)", value: 1, icon: EtherImg, disabled: false },
+  { label: "Sepolia (ETH Testnet)", value: 1, icon: EtherImg, disabled: false },
   { label: "SUI Testnet", value: 2, icon: SUIImg, disabled: false },
   { label: "Polkadot Testnet", value: 3, icon: PolkadotImg, disabled: true },
   { label: "Oasis Testnet", value: 4, icon: OasisImg, disabled: false },
